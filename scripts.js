9becdb5bf73b344efef880cf0e84917e168d2f58
Billy Bit <zscot45@eq.edu.au>
2026-03-16 21:24:16 +1000

    Update scripts.js

--- scripts.js
+++ scripts.js
@@ -1,18 +1,30 @@
-const planet = document.getElementById('planet-glasshouse');
-const sidebar = document.getElementById('sidebar');
 const overlay = document.getElementById('sidebar-overlay');
-const closeBtn = document.getElementById('sidebar-close');
 
-function openSidebar() {
+const planetDoofus    = document.getElementById('planet-doofus');
+const sidebarDoofus   = document.getElementById('sidebar-doofus');
+const closeDoofus     = document.getElementById('close-doofus');
+
+const planetGlass     = document.getElementById('planet-glasshouse');
+const sidebarGlass    = document.getElementById('sidebar-glasshouse');
+const closeGlass      = document.getElementById('close-glasshouse');
+
+
+function openSidebar(sidebar) {
     sidebar.classList.add('open');
     overlay.classList.add('open');
 }
 
-function closeSidebar() {
-    sidebar.classList.remove('open');
+function closeAll() {
+    sidebarDoofus.classList.remove('open');
+    sidebarGlass.classList.remove('open');
     overlay.classList.remove('open');
 }
 
-planet.addEventListener('click', openSidebar);
-closeBtn.addEventListener('click', closeSidebar);
-overlay.addEventListener('click', closeSidebar);
+
+planetDoofus.addEventListener('click', function() { openSidebar(sidebarDoofus); });
+closeDoofus.addEventListener('click', closeAll);
+
+planetGlass.addEventListener('click', function() { openSidebar(sidebarGlass); });
+closeGlass.addEventListener('click', closeAll);
+
+overlay.addEventListener('click', closeAll);
